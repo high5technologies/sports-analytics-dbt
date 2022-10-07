aa5b5e57f04cbe875d5f20b99dc3163beae02f87
high5technologies@gmail.com <high5technologies@gmail.com>
2022-10-07 04:39:27 +0000

    NA raptor_ml_pick

--- models/nba/2_conf/nba__conf_linestar_ownership.sql
+++ models/nba/2_conf/nba__conf_linestar_ownership.sql
@@ -2,19 +2,21 @@
     tags=["nba"]
 ) }}
 
-SELECT  
-    linestar_key
-    ,game_date
-    ,cast(pid as INT64) as pid
-    ,dfs_source
-    ,cast(dfs_contest_id as INT64) as dfs_contest_id
-    ,linestar_type
-    ,cast(player_id as INT64) as player_id_linestar
-    ,player_name
-    ,cast(owned as numeric) as owned
-    ,player_pos
-    ,team
-    ,cast(player_salary as INT64) as player_salary
-    ,load_datetime
-FROM {{ source('nba_raw','raw_linestar_ownership') }}
-QUALIFY row_number() over (partition by linestar_key order by load_datetime desc) = 1
+
+    SELECT  
+        linestar_key
+        ,game_date
+        ,cast(pid as INT64) as pid
+        ,case when dfs_source = 'DraftKings' then 'dk' when dfs_source = 'FanDuel' then 'fd' end as dfs_source
+        ,cast(dfs_contest_id as INT64) as dfs_contest_id
+        ,linestar_type
+        ,cast(player_id as INT64) as player_id_linestar
+        ,player_name
+        ,cast(owned as numeric) as owned
+        ,player_pos
+        ,case when player_pos not in ('PG','SG','SF','PF','C') then null else player_pos end as player_pos_adj
+        ,team
+        ,cast(player_salary as INT64) as player_salary
+        ,load_datetime
+    FROM {{ source('nba_raw','raw_linestar_ownership') }}
+    QUALIFY row_number() over (partition by linestar_key order by load_datetime desc) = 1
--- models/nba/2_conf/nba__conf_nbacom_game_player.sql
+++ models/nba/2_conf/nba__conf_nbacom_game_player.sql
@@ -18,26 +18,27 @@ SELECT
     ,case when position = '' then FALSE when position != '' then TRUE end as starter_flag
     ,comment
     ,jersey_num
-    ,minutes
-    ,field_goals_made
-    ,field_goals_attempted
-    ,field_goals_percentage
-    ,three_pointers_made
-    ,three_pointers_attempted
-    ,three_pointers_percentage
-    ,free_throws_made
-    ,free_throws_attempted
-    ,free_throws_percentage
-    ,rebounds_offensive
-    ,rebounds_defensive
-    ,rebounds_total
-    ,assists
-    ,steals
-    ,blocks
-    ,turnovers
-    ,fouls_personal
-    ,points
-    ,plus_minus_points
+    ,minutes as minutes_string
+    ,case when minutes = '' then 0 else (cast(split(minutes,':')[ORDINAL(1)] as INT64) * 60) + cast(split(minutes,':')[ORDINAL(2)] as INT64) end as seconds_played
+    ,case when minutes = '' then 0 else (cast(split(minutes,':')[ORDINAL(1)] as INT64)) + (cast(split(minutes,':')[ORDINAL(2)] as INT64) / 60) end as minutes_played	
+    ,cast(field_goals_made as INT64) as field_goals_made
+    ,cast(field_goals_attempted as INT64) as field_goals_attempted
+    ,cast(field_goals_percentage as NUMERIC) as field_goals_percentage
+    ,cast(three_pointers_made as INT64) as three_pointers_made
+    ,cast(three_pointers_attempted as INT64) as three_pointers_attempted
+    ,cast(three_pointers_percentage as NUMERIC) as three_pointers_percentage
+    ,cast(free_throws_made as INT64) as free_throws_made
+    ,cast(free_throws_attempted as INT64) as free_throws_attempted
+    ,cast(free_throws_percentage as NUMERIC) as free_throws_percentage
+    ,cast(rebounds_defensive as INT64) as rebounds_defensive
+    ,cast(rebounds_total as INT64) as rebounds_total
+    ,cast(assists as INT64) as assists
+    ,cast(steals as INT64) as steals
+    ,cast(blocks as INT64) as blocks
+    ,cast(turnovers as INT64) as turnovers
+    ,cast(fouls_personal as INT64) as fouls_personal
+    ,cast(points as INT64) as points
+    ,cast(plus_minus_points as INT64) as plus_minus_points
     ,load_datetime
     --, row_number() over (partition by game_player_key order by load_datetime desc) as dedup
 FROM {{ source('nba_raw','raw_nbacom_game_player') }}
